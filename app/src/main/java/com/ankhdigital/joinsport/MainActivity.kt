15eab66db06e6faf3438151a32ae123586484292
Iqbal <muhiqbal1290@gmail.com>
2022-08-13 21:20:37 +0700

    Done Slicing All Menu From Figma

--- app/src/main/java/com/ankhdigital/joinsport/MainActivity.kt
+++ app/src/main/java/com/ankhdigital/joinsport/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.ankhdigital.joinsport
 
-import android.graphics.Color
-import android.graphics.LinearGradient
-import android.graphics.Shader
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextPaint
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ankhdigital.joinsport.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/ankhdigital/joinsport/ui/upload/UploadFragment.kt
@@ -0,0 +1,35 @@
+package com.ankhdigital.joinsport.ui.upload
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.ankhdigital.joinsport.databinding.FragmentUploadBinding
+
+class UploadFragment : Fragment() {
+
+    private var _binding: FragmentUploadBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentUploadBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
