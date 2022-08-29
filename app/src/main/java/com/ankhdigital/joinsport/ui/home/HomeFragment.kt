0731de4f7343231d63a63870f3bb5d9bef90d340
Iqbal <muhiqbal1290@gmail.com>
2022-08-30 05:24:16 +0700

    Add post action

--- app/src/main/java/com/ankhdigital/joinsport/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ankhdigital/joinsport/ui/home/HomeFragment.kt
@@ -1,17 +1,21 @@
 package com.ankhdigital.joinsport.ui.home
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.PopupWindow
+import android.widget.Toast
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.ankhdigital.joinsport.DetailActivity
+import com.ankhdigital.joinsport.R
 import com.ankhdigital.joinsport.databinding.DemoFragmentHomeBinding
-import com.ankhdigital.joinsport.databinding.FragmentHomeBinding
-import com.ankhdigital.joinsport.databinding.ItemlistMainTimelineBinding
+import com.ankhdigital.joinsport.databinding.MenuActionBinding
+import java.lang.Exception
 
 class HomeFragment : Fragment() {
 
@@ -32,6 +36,15 @@ class HomeFragment : Fragment() {
     ): View {
 
         _binding = DemoFragmentHomeBinding.inflate(inflater, container, false)
+//        val popupInflater = requireActivity().applicationContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//        val popupBind = MenuActionBinding.bind(popupInflater)
+//
+//        val popupWindow = PopupWindow(
+//            popupBind.root, 126,
+//            89, true
+//        ).apply { contentView.setOnClickListener { dismiss() } }
+//
+//        binding.ivPostSettings.setOnClickListener(popupWindow::showAsDropDown)
         val root: View = binding.root
 
         return root
@@ -41,11 +54,44 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
-            ivPostImg.setOnClickListener{
+            ivPostImg.setOnClickListener {
                 val goToPostDetail = Intent(requireContext(), DetailActivity::class.java)
                 startActivity(goToPostDetail)
             }
+
+            ivPostSettings.setOnClickListener {
+                popUpMenu()
+            }
         }
+    }
+
+    private fun popUpMenu() {
+        val popupMenu = PopupMenu(requireContext(), binding.ivPostSettings)
+        popupMenu.inflate(R.menu.popup_menu)
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_edit -> {
+                    Toast.makeText(requireContext(), "Edited", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.menu_delete -> {
+                    Toast.makeText(requireContext(), "Deleted", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> true
+            }
+        }
+
+        try {
+            val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+            popup.isAccessible = true
+            val menu = popup.get(popupMenu)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            popupMenu.show()
+        }
+
     }
 
     override fun onDestroyView() {
